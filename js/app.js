2673df7287c2ea00da55d4fc73e05536c3b34d83
gestaodecustohrdjsn <gestaodecustohrdjsn@gmail.com>
2026-06-17 14:41:25 -0400

    Add API and DOM element references in app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
 
+const API =
+"https://script.google.com/macros/s/AKfycbzK5alad7lCUe3lK913lCfgH7C8DlP1_7URg-l6VW_4kdgIWVkj3DRAxpGIjU0bVt9nig/exec";
+
+const telaHuddle =
+document.getElementById("tela-huddle");
+
+const telaSetor =
+document.getElementById("tela-setor");
+
+const telaPerguntas =
+document.getElementById("tela-perguntas");
+
+const listaHuddles =
+document.getElementById("lista-huddles");
+
+const listaSetores =
+document.getElementById("lista-setores");
+
+const listaPerguntas =
+document.getElementById("lista-perguntas");
+
+carregarHuddles();
